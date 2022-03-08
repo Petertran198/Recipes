7a9a6fc89c7e8afb3690aaa94140f41bc7198f92
petertran98 <Petertran198@gmail.com>
2022-03-07 16:07:33 -0800

    Added way to conditionally  render  no-recipe template if no recipe is clicked using  ng-template and if else directives

--- src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -7,7 +7,13 @@ import { Recipe } from '../../recipe.model';
   styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent implements OnInit {
-  @Input() recipes: Recipe[];
+  @Input() recipe: Recipe;
+  @Output() selectedRecipeElement = new EventEmitter<void>();
+
   constructor() {}
   ngOnInit(): void {}
+
+  handleClicked() {
+    this.selectedRecipeElement.emit();
+  }
 }
